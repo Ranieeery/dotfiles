4a0b4a29859ab87ccc1cd73cacea1af87761f08b
Ranieeery <raniery2003@hotmail.com>
2025-01-07 14:09:30 -0300

    feat: add utility scripts for repository cloning and system configuration

---
+++ windows/utility/KillProcessUsingPort.ps1
@@ -0,0 +1,10 @@
+netstat -ano | findstr :8080
+
+# TCP     0.0.0.0:8080    0.0.0.0:0   LISTENING   2660
+# TCP     [::]:8080       [::]:0      LISTENING   2660
+
+taskkill /PID 2660 /F
+
+# with npm kill-port
+
+npx kill-port 8080
