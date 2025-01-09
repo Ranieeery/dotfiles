29b25e72c3ff217cb1caa338ac1207017353004b
Ranieeery <raniery2003@hotmail.com>
2025-01-08 22:01:28 -0300

    feat: enhance repository cloning script and update system configuration with git-filter-repo

--- windows/utility/clonerepo.ps1
+++ windows/utility/clonerepo.ps1
@@ -1,5 +1,6 @@
 git rev-parse --git-dir
 git subtree split -P "path/here" -b temp-branch
+New-Item "C:\path\to\repo"
 Set-Location "C:\path\to\repo"
 
 git init
@@ -7,5 +8,10 @@ git pull "path/here" temp-branch
 git remote add origin "linktorepo.git"
 git push -u origin main
 
+Set-Location "firstpath"
 git branch -D temp-branch
 
+git filter-repo --invert-paths --path "path/here"
+git push origin --force
+
+# git push --set-upstream origin main --force
--- windows/utility/systemConfig.ps1
+++ windows/utility/systemConfig.ps1
@@ -34,5 +34,6 @@ choco update
 # Need to install Python
 
 pip install spotdl
+pip install git-filter-repo
 ## spotdl --cookie-file cookies.txt https://open.spotify.com/playlist/1PuWahEBkGv7us44Anfd76?si=b5371f1301754d63
 
