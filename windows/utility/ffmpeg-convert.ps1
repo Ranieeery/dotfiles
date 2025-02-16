f7e45844df7c60954ed6aed80e38fc5f79e2a063
Raniery <102702376+Ranieeery@users.noreply.github.com>
2025-02-16 13:29:21 -0300

    feat: update ffmpeg-convert.ps1

--- windows/utility/ffmpeg-convert.ps1
+++ windows/utility/ffmpeg-convert.ps1
@@ -3,3 +3,9 @@ ffmpeg -i input.mp4 -c copy output.mkv
 
 # Convert video to mp4 to mkv with subtitles
 ffmpeg -i input.mp4 -i input.srt -c copy -c:s mov_text output.mkv
+
+# Convert video to audio file
+ffmpeg -i input.mp4 -vn -c:a libmp3lame -q:a 0 output.mp3
+
+# Resize image scale
+ffmpeg -i input.png -vf "scale=iw/2:ih/2" output.png
