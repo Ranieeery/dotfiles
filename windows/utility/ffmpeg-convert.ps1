cd96d821062bd1a12030e05f56c7339b87868486
Ranieeery <raniery2003@hotmail.com>
2025-03-10 14:09:36 -0300

    feat: add image size reduction command to ffmpeg-convert.ps1

--- windows/utility/ffmpeg-convert.ps1
+++ windows/utility/ffmpeg-convert.ps1
@@ -10,5 +10,8 @@ ffmpeg -i input.mp4 -vn -c:a libmp3lame -q:a 0 output.mp3
 # Resize image scale
 ffmpeg -i input.png -vf "scale=iw/2:ih/2" output.png
 
+# Reduce image size
+ffmpeg -i input.png -compression_level 9 -pred mixed -vf "format=rgba,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" output.png
+
 # Cut video in HH:MM:SS format
 ffmpeg -i input.mp4 -t 00:00:02 output.mp4
