f1837c08d4f6e4e59db47f94cb0d0bc034229763
Ranieeery <raniery2003@hotmail.com>
2025-04-22 10:41:05 -0300

    feat: update Oh My Zsh installation command to use curl instead of wget

--- linux/setup/setup-ubuntu.sh
+++ linux/setup/setup-ubuntu.sh
@@ -82,7 +82,7 @@ echo "https://docs.docker.com/desktop/install/ubuntu/"
 
 # Instalar Zsh e Oh My Zsh
 sudo apt install zsh -y
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
