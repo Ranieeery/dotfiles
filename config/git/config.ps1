065aed2e07a5ab019aecf533f9774c808ce5096b
Ranieeery <raniery2003@hotmail.com>
2026-05-28 13:58:14 -0300

    feat: update Git configuration to use core.autocrlf input and streamline SSH setup

--- config/git/config.ps1
+++ config/git/config.ps1
@@ -3,11 +3,12 @@ winget install --id Git.Git -e --source winget
 git config --global user.name "Ranieeery"
 git config --global user.email "raniery2003@hotmail.com"
 git config --global init.defaultBranch main
-git config --global core.autocrlf false
+git config --global core.autocrlf input
 
 ssh-keygen -t ed25519 -C "raniery2003@hotmail.com"
 Get-Service -Name ssh-agent | Set-Service -StartupType Manual 
 Start-Service ssh-agent
 Get-Content ~/.ssh/id_ed25519.pub | clip
 
+ssh -T git@github.com
 # Get-Service -Name ssh-agent | Select-Object StartType
--- config/git/config.sh
+++ config/git/config.sh
@@ -1,14 +1,10 @@
-apt-get install git
-# pacman -S git
-
 git config --global user.name "Ranieeery"
 git config --global user.email "raniery2003@hotmail.com"
 git config --global init.defaultBranch main
-git config --global core.autocrlf false
 
 ssh-keygen -t ed25519 -C "raniery2003@hotmail.com"
-
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-
 cat ~/.ssh/id_ed25519.pub
+
+ssh -T git@github.com
